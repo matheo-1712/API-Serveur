7d507ee3000b77215a69fa17d52dae9a6a9cdea9
matheo-prd <perod.matheo@gmail.com>
2024-09-04 23:45:12 +0200

    MAJ Automatique de l'API à chaque changement de JSON

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Définition de l'API
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 // Configuration du port d'écoute du serveur
 const port = 3000;
@@ -33,3 +34,20 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Une erreur est survenue sur le serveur');
 });
+
+// En cas de changement dans les JSON redémarre le serveur
+fs.watchFile('./data/serveurs.json', (curr, prev) => {
+    console.log('Le fichier serveurs.json a été modifié');
+    delete require.cache[require.resolve('./data/serveurs.json')];
+});
+
+fs.watchFile('./data/actif.json', (curr, prev) => {
+    console.log('Le fichier actif.json a été modifié');
+    delete require.cache[require.resolve('./data/actif.json')];
+});
+
+fs.watchFile('./data/serveurInvestisseur.json', (curr, prev) => {
+    console.log('Le fichier serveurInvestisseur.json a été modifié');
+    delete require.cache[require.resolve('./data/serveurInvestisseur.json')];
+});
+
